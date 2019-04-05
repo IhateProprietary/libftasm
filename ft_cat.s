2fbfbb50b3b361a68b049d54a012b73be9fb104f
IhateProprietary <death2511@hotmail.com>
2019-04-05 17:19:29 +0200

    now testing output, string and file descriptor likewise

--- ft_cat.s
+++ ft_cat.s
@@ -7,16 +7,20 @@ _ft_cat:
 	push rbp
 	lea rbp, [rsp]
 	push rdi
-L1:	
+L1:
+	;; READ SYSCALL
 	mov rax, 0x2000003
 	lea rsi, [BUFFER]
 	mov rdx, 4096
 	mov rdi, [rsp]
 	syscall
 	jc END
+	test rax, rax
+	jz END
 	lea rsi, [BUFFER]
 	mov rdx, rax
 	mov rdi, 1
+	;; WRITE SYSCALL
 	mov rax, 0x2000004
 	syscall
 	jc END
--- test.py
+++ test.py
@@ -41,42 +41,48 @@ FUNCTION = {
             [CTYPE.VOID, ARGV.DST, 1],
             [CTYPE.SIZE_T, ARGV.SIZE, 0]
         ],
-        "cdefines": [DEF.MEMORY, DEF.ORIGINAL]
+        "cdefines": [DEF.MEMORY, DEF.ORIGINAL],
+        "test_type": 0
     },
     "isalnum": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.INT, ARGV.INT, 0],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "isalpha": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.INT, ARGV.INT, 0],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "isascii": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.INT, ARGV.INT, 0],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "isdigit": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.INT, ARGV.INT, 0],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "isprint": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.INT, ARGV.INT, 0],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "memchr": {
         "type": [CTYPE.VOID, 1],
@@ -85,7 +91,8 @@ FUNCTION = {
             [CTYPE.INT, ARGV.INT, 0],
             [CTYPE.SIZE_T, ARGV.SIZE, 0]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "memcmp": {
         "type": [CTYPE.INT, 0],
@@ -94,7 +101,8 @@ FUNCTION = {
             [CTYPE.VOID, ARGV.CMP, 1],
             [CTYPE.SIZE_T, ARGV.SIZE, 0]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "memcpy": {
         "type": [CTYPE.VOID, 1],
@@ -103,7 +111,8 @@ FUNCTION = {
             [CTYPE.VOID, ARGV.SRC, 1],
             [CTYPE.SIZE_T, ARGV.SIZE, 0]
         ],
-        "cdefines": [DEF.PTR, DEF.MEMORY, DEF.ORIGINAL]
+        "cdefines": [DEF.PTR, DEF.MEMORY, DEF.ORIGINAL],
+        "test_type": 0
     },
     "memset": {
         "type": [CTYPE.VOID, 1],
@@ -112,7 +121,8 @@ FUNCTION = {
             [CTYPE.INT, ARGV.INT, 0],
             [CTYPE.SIZE_T, ARGV.SIZE, 0]
         ],
-        "cdefines": [DEF.MEMORY, DEF.ORIGINAL]
+        "cdefines": [DEF.MEMORY, DEF.ORIGINAL],
+        "test_type": 0
     },
     "strchr": {
         "type": [CTYPE.CHAR, 1],
@@ -120,7 +130,8 @@ FUNCTION = {
             [CTYPE.CHAR, ARGV.SRC, 1],
             [CTYPE.INT, ARGV.INT, 0]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "strcmp": {
         "type": [CTYPE.INT, 0],
@@ -128,52 +139,59 @@ FUNCTION = {
             [CTYPE.CHAR, ARGV.SRC, 1],
             [CTYPE.CHAR, ARGV.CMP, 1]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "strdup": {
         "type": [CTYPE.CHAR, 1],
         "argv": [
             [CTYPE.CHAR, ARGV.SRC, 1]
         ],
-        "cdefines": [DEF.STRING, DEF.PTR, DEF.ORIGINAL]
+        "cdefines": [DEF.STRING, DEF.PTR, DEF.ORIGINAL],
+        "test_type": 0
     },
     "strlen": {
         "type": [CTYPE.SIZE_T, 0],
         "argv": [
             [CTYPE.CHAR, ARGV.SRC, 1],
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "tolower": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.CHAR, ARGV.INT, 0]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     "toupper": {
         "type": [CTYPE.INT, 0],
         "argv": [
             [CTYPE.CHAR, ARGV.INT, 0]
         ],
-        "cdefines": [DEF.RETURN, DEF.ORIGINAL]
+        "cdefines": [DEF.RETURN, DEF.ORIGINAL],
+        "test_type": 0
     },
     ### BELOW FUNCTION CANNOT BE TESTED AGAINST ORIGINAL
     ### ONLY OUTPUT WILL BE TESTED
-    # "puts": {
-    #     "type": [CTYPE.INT, 0],
-    #     "argv": [
-    #         [CTYPE.CHAR, ARGV.SRC, 1]
-    #     ],
-    #     "cdefines": []
-    # },
-    # "cat": {
-    #     "type": [CTYPE.VOID, 0],
-    #     "argv": [
-    #         [CTYPE.INT, ARGV.FD_READ, 0]
-    #     ],
-    #     "cdefines": []
-    # },
+    "puts": {
+        "type": [CTYPE.INT, 0],
+        "argv": [
+            [CTYPE.CHAR, ARGV.SRC, 1]
+        ],
+        "cdefines": [DEF.STRING],
+        "test_type": 1
+    },
+    "cat": {
+        "type": [CTYPE.VOID, 0],
+        "argv": [
+            [CTYPE.INT, ARGV.FD_READ, 0]
+        ],
+        "cdefines": [],
+        "test_type": 2
+    },
 }
 
 TEMPLATE="""#include <stdio.h>
@@ -222,7 +240,7 @@ int     main(int ac, char **av)
 #endif
 
 
-#ifdef __MEMORY__
+#if defined(__ORIGINAL__) && defined(__MEMORY__)
 # ifdef __PTR__
     assert(!memcmp(ret, src, bufsize));
 # else
@@ -230,7 +248,7 @@ int     main(int ac, char **av)
 # endif
 #endif
 
-#ifdef __STRING__
+#if defined(__ORIGINAL__) && defined(__STRING__)
 # ifdef __PTR__
     assert(!strcmp(ret, src));
 # else
@@ -241,7 +259,6 @@ int     main(int ac, char **av)
 #ifdef __RETURN__
     assert(ret == oret);
 #endif
-    dprintf(1, "TEST %s OK\\n", __FILE__);
     return (0);
 }}
 """
@@ -254,7 +271,7 @@ def format_func_argument(_argv_type):
 
 if __name__ == "__main__":
     fd = os.open("/dev/urandom", os.O_RDONLY)
-    optlist, arg = getopt.gnu_getopt(sys.argv, "p:s:n:", longopts=["suffix=", "prefix=", "number=", "folder="])
+    optlist, arg = getopt.gnu_getopt(sys.argv, "p:s:n:", longopts=["suffix=", "prefix=", "number="])
     folder = "./"
     suffix = "_"
     prefix = "test_"
@@ -264,10 +281,8 @@ if __name__ == "__main__":
             suffix = _arg
         elif opt == "--prefix" or opt == "-p":
             prefix = _arg
-        elif opt == "number" or opt == "-n":
+        elif opt == "--number" or opt == "-n":
             n = int(_arg)
-        elif opt == "folder":
-            folder = _arg
 
     for _ in range(0, n):
         for key, value in FUNCTION.items():
@@ -276,12 +291,21 @@ if __name__ == "__main__":
                  format_func_argument(_argv_type))
                 for _type, _argv_type, _ptr in value["argv"]
             ]
-            sizebuf = random.randrange(0, 16384+1, 8)
-            buf1 = "".join(["\\x{:02x}".format(x) for x in os.read(fd, sizebuf)])
-            buf2 = "".join(["\\x{:02x}".format(x) for x in os.read(fd, sizebuf)])
+            sizebuf = random.randrange(32, 16384+1, 8)
+            ## get randomized buffer
+            _buf1 = os.read(fd, sizebuf)
+            _buf2 = os.read(fd, sizebuf)
+            ## writing raw string in hexadecimal
+            buf1 = "".join(["\\x{:02x}".format(x) for x in _buf1])
+            buf2 = "".join(["\\x{:02x}".format(x) for x in _buf2])
+            ## filename
             filename = folder+prefix+key+suffix+str(_)
+            ## if string, put a 0 somewhere, assuming there isn't one already in /dev/urandom
+            _cut = random.randrange(0, sizebuf, 8)
             z = os.open(filename + ".c", os.O_TRUNC | os.O_CREAT | os.O_WRONLY, 0o644)
+            ## if testing with an int
             rchar=random.randrange(1, 127)
+            ## outputing format template in a file
             os.write(z, TEMPLATE.format(
                 func=key,
                 _type = value["type"][0].value,
@@ -289,15 +313,43 @@ if __name__ == "__main__":
                 argv=", ".join([x[0] for x in argv]),
                 arg=", ".join([x[1] for x in argv]).format(dst = "__dst", char = rchar),
                 oarg=", ".join([x[1] for x in argv]).format(dst = "__odst", char = rchar),
-                rdm = random.randrange(1, sizebuf, 8),
+                rdm = _cut,
                 buffer1=buf1,
                 buffer2=buf2,
             ).encode("UTF-8"))
             os.close(z)
             cmd = "gcc {filename}.c -o {filename} {DEFINES} -L. -lfts".format(filename=filename, DEFINES=" ".join([x.value for x in value["cdefines"]]))
-            print(cmd)
             cmd = shlex.split(cmd)
             subprocess.run(cmd)
-            _ret = subprocess.run(filename, capture_output=True)
-            print(_ret.stdout, _ret.stderr, sep='\n')
-                
+            ## testing against original
+            if value["test_type"] == 0:
+                _ret = subprocess.run(filename, capture_output=True)
+                if _ret.returncode != 0:
+                    print(filename + " KO")
+                else:
+                    print(filename + " OK")
+            ## testing output against our own, assuming below is a string
+            elif value["test_type"] == 1:
+                _ret = subprocess.Popen(filename, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                ret = _ret.communicate(input=_buf1)[0]
+                __cut = _buf1.find(0)
+                if __cut != -1:
+                    _cut = _cut if _cut < __cut else __cut
+                if ret != _buf1[:_cut]:
+                    _fd = os.open(key+str(_)+"_output", os.O_CREAT | os.O_WRONLY, 0o644)
+                    os.write(_fd, ret)
+                    os.close(_fd)
+                    _fd = os.open(key+str(_)+"original_output", os.O_CREAT | os.O_WRONLY, 0o644)
+                    os.write(_fd, _buf1[:_cut])
+                    os.close(_fd)
+                    print(filename + " KO")
+                else:
+                    print(filename + " OK")
+            ## testing output against our own, assuming it read everything including '\0'
+            elif value["test_type"] == 2:
+                _ret = subprocess.Popen(filename, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                ret = _ret.communicate(input=_buf1)[0]
+                if ret != _buf1:
+                    print(filename + " KO")
+                else:
+                    print(filename + " OK")
